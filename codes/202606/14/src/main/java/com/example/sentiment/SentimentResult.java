95529ff0d19785b4991194e0ecf8f6d0a34b7bdf
j3y3h0 <mlpzaq123456@naver.com>
2026-06-14 09:31:47 +0900

    Save: 202606140931

---
+++ codes/202606/14/src/main/java/com/example/sentiment/SentimentResult.java
@@ -0,0 +1,11 @@
+package com.example.sentiment;
+
+/**
+ * 텍스트의 감성 분석 결과를 나타내는 열거형입니다.
+ * 긍정, 부정, 중립 세 가지 감성을 정의합니다.
+ */
+public enum SentimentResult {
+    POSITIVE, // 긍정적인 감성
+    NEGATIVE, // 부정적인 감성
+    NEUTRAL   // 중립적인 감성
+}
